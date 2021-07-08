ed92676459e720d4c16ed4fb5b17a1d5307938d1
Natalia151283 <2371340@gmail.com>
2021-07-08 21:59:30 +0300

    hw-11-3/4

--- src/index.js
+++ src/index.js
@@ -6,53 +6,6 @@ startBnt : document.querySelector("button[data-action-start]"),
 stoptBnt : document.querySelector("button[data-action-stop]")
 };
 
-// class CountdownTimer {
-//   constructor({ selector, targetDate }) {
-//     this.targetDate = targetDate;
-//     this.selector = document.querySelector(selector);
-//     this.start();
-//   }
-
-//   start() {
-//     if (this.isActive) {
-//       return;
-//     }
-   
-//     this.isActive = true;
-
-//     this.intervalId = setInterval(() => {
-//       const currentTime = Date.now();
-//       const deltaTime = this.targetDate -currentTime;
-//       const time = this.getTime(deltaTime);
-
-//       console.log(time);
-
-//       this.selector.querySelector('span[data-value="days"]').textContent = time.days;
-//       this.selector.querySelector('span[data-value="hours"]').textContent = time.hours;
-//       this.selector.querySelector('span[data-value="mins"]').textContent = time.mins;
-//       this.selector.querySelector('span[data-value="secs"]').textContent = time.secs;
-//     }, 1000);
-//   }
-
-
-//   getTime(time) {
-//     const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-//     const hours = this.pad(
-//       Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-//     );
-//     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-//     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-//     return { days, hours, mins, secs };
-//   }
-
-//   pad(value) {
-//     return String(value).padStart(2, "0");
-//   }
-
-// }
-
-
-// new CountdownTimer({selector: "#timer-1", targetDate: new Date("Jul 17, 2021")});
 class CountdownTimer {
   constructor({ selector, targetDate }){
     this.intervalId = null;
