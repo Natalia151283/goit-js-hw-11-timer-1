c17ab8551f385a3a8364ca3992c76769283f402f
Natalia151283 <2371340@gmail.com>
2021-07-08 20:55:54 +0300

    hw-11-3-1

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -353,7 +353,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55941" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61259" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
